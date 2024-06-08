9c5920c5932e76f326bfabedb8cf9c2865cf397b
blood <asteroid.xyz.1000@gmail.com>
2024-06-08 18:43:08 +0900

    Appの抽象化

--- src/renderer/app/App.tsx
+++ src/renderer/app/App.tsx
@@ -6,12 +6,29 @@ import HelloWorld from '../features/HelloWorld/HelloWorld'
 import '../styles.css' // スタイルが必要であればインポートします
 import SurvivalTypeScriptContainer from '../features/SurvivalTypeScript/SurvivalTypeScript'
 import TicTacToe from '../features/TicTacToe/TicTacToe'
-import { Tab, Box, AppBar } from '@mui/material'
+import { Tab, Box, AppBar, Divider, Stack } from '@mui/material'
 import { TabContext, TabList, TabPanel } from '@mui/lab'
 import InputField from '../features/InputField/InputField'
+import DiaryApp from '../features/DiaryApp/DiaryApp'
 
 const App: React.FC = () => {
   const [currentTab, setCurrentTab] = React.useState<string>('1')
+
+  const contentsList = [
+    { label: 'HelloWorld', content: <HelloReact /> },
+    {
+      label: 'Toggle HelloWorld',
+      content: (
+        <Provider store={store}>
+          <HelloWorld />
+        </Provider>
+      ),
+    },
+    { label: 'Good Button', content: <SurvivalTypeScriptContainer /> },
+    { label: 'TicTacToe', content: <TicTacToe /> },
+    { label: 'Input Field', content: <InputField /> },
+    { label: 'DiaryApp', content: <DiaryApp /> },
+  ]
 
   const handleChange = (event: React.SyntheticEvent, newValue: string) => {
     setCurrentTab(newValue)
@@ -19,47 +36,38 @@ const App: React.FC = () => {
 
   return (
     <TabContext value={currentTab}>
-      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-        <TabList onChange={handleChange} aria-label='simple tabs example'>
-          <Tab label='HelloWorld' value='1' />
-          <Tab label='Toggle HelloWorld' value='2' />
-          <Tab label='Good Button' value='3' />
-          <Tab label='TicTacToe' value='4' />
-          <Tab label='Input Field' value='5' />
-        </TabList>
-      </Box>
-      <TabPanel value='1'>
-        <div className='bg-blue-500 text-white p-4 max-w-sm mx-auto mt-20'>
-          <h1 className='text-4xl font-bold'>Hello from React and Tailwind!</h1>
-          <p className='mt-2 text-lg'>
-            This is a simple example showing Tailwind CSS with React.
-          </p>
-        </div>
-      </TabPanel>
-      <TabPanel value='2'>
-        <div>
-          <Provider store={store}>
-            <HelloWorld />
-          </Provider>
-        </div>
-      </TabPanel>
-      <TabPanel value='3'>
-        <div>
-          <SurvivalTypeScriptContainer />
-        </div>
-      </TabPanel>
-      <TabPanel value='4'>
-        <div>
-          <TicTacToe />
-        </div>
-      </TabPanel>
-      <TabPanel value='5'>
-        <div>
-          <InputField />
-        </div>
-      </TabPanel>
+      <Stack>
+        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+          <TabList onChange={handleChange} aria-label='simple tabs example'>
+            {contentsList.map((content, index) => (
+              <Tab
+                key={index}
+                label={content.label}
+                value={(index + 1).toString()}
+              />
+            ))}
+          </TabList>
+        </Box>
+        <Divider />
+        {contentsList.map((content, index) => (
+          <TabPanel key={index} value={(index + 1).toString()}>
+            {content.content}
+          </TabPanel>
+        ))}
+      </Stack>
     </TabContext>
   )
 }
 
 export default App
+
+const HelloReact = () => {
+  return (
+    <div className='bg-blue-500 text-white p-4 max-w-sm mx-auto mt-20'>
+      <h1 className='text-4xl font-bold'>Hello from React and Tailwind!</h1>
+      <p className='mt-2 text-lg'>
+        This is a simple example showing Tailwind CSS with React.
+      </p>
+    </div>
+  )
+}
