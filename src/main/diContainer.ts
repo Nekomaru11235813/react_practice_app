da12ee28f3f9463d6ff3cea912589f16cc944127
blood <asteroid.xyz.1000@gmail.com>
2024-12-16 23:29:58 +0900

    タグのバリデーションをDIコンテナへ追加

--- src/main/diContainer.ts
+++ src/main/diContainer.ts
@@ -4,12 +4,17 @@ import { container } from 'tsyringe'
 import { ArticleValidate } from './feature/diaryApp/domain/entity/validate/articleValidate'
 import { IdValidate } from './common/idValidate'
 import { DiaryRepositorySQlite } from './feature/diaryApp/repository/sqlite3/diaryRepositorySqlite'
+import { TagValidate } from './feature/diaryApp/domain/entity/validate/tagValidate'
 
 container.register('ArticleValidate', {
   useClass: ArticleValidate,
 })
 container.register('IdValidate', {
   useClass: IdValidate,
+})
+
+container.register('TagValidate', {
+  useClass: TagValidate,
 })
 
 container.register('DiaryRepository', {
